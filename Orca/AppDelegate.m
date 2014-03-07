ccbbf5b818af13b59468090dfbcfcb828dc83930
Dalton Cherry <daltoniam@gmail.com>
2014-03-06 17:58:32 -0800

    some UI stuff
    some UI stuff

--- Orca/AppDelegate.m
+++ Orca/AppDelegate.m
@@ -12,14 +12,23 @@
 
 @implementation AppDelegate
 
+///////////////////////////////////////////////////////////////////////////////////////
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
     self.pcap = [[PacketCapture alloc] init];
     NSArray *interfaces = [self.pcap getInterfaces];
     for(NSString *interface in interfaces)
         NSLog(@"inteface: %@", interface);
-    [self.pcap capturePackets:[interfaces firstObject]];
+    [self.popButton addItemsWithTitles:interfaces];
 }
+///////////////////////////////////////////////////////////////////////////////////////
+-(IBAction)startCapture:(id)sender
+{
+    NSString *interface = self.popButton.selectedItem.title;
+    NSLog(@"starting capture on: %@",interface);
+    [self.pcap capturePackets:interface];
+}
+///////////////////////////////////////////////////////////////////////////////////////
+
 
 @end
